a88adca174f9070642dc3202f4a60dad0149bd3a
V-Martyanov <martyanov@phystech.edu>
2021-09-07 23:03:52 +0300

    добавил пробелы

--- 1A/main.cpp
+++ 1A/main.cpp
@@ -3,33 +3,37 @@
 #include <iomanip>
 #include <algorithm>
 using namespace std;
+const int size = 10;
 int main()
 { 
-    double x1,x2,y1,y2;
-    cin>>x1>>y1>>x2>>y2;
-    if (x1 > x2)
-    {
-        double t1=x1,t2=y1;
-        x1=x2;
-        y1=y2;
-        x2=t1;
-        y2=t2;
-    }
-    std::cout << std::fixed << std::showpoint;
-    std::cout << std::setprecision(10);
-    double X1,X2;
-    double D = sqrt(((2*x1*y2*y2 - 2*x2*y1*y1))*((2*x1*y2*y2 - 2*x2*y1*y1))- 4*(y2*y2-y1*y1)*(x1*x1*y2*y2-x2*x2*y1*y1));
-    X1=((2*x1*y2*y2 - 2*x2*y1*y1)+D)/(2*((y2*y2-y1*y1)));
-    X2=((2*x1*y2*y2 - 2*x2*y1*y1)-D)/(2*((y2*y2-y1*y1)));
-    if( X1 > x1 && X1 < x2)
-    {
-        cout << X1 << '\n';
-        return 0;
-    }
-    else
-    {
-        cout << X2 << '\n';
-        return 0;
-    }
-    return 0;
+  double x1;
+  double x2;
+  double y1;
+  double y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+  if (x1 > x2)
+  {
+    double t1 = x1;
+    double t2 = y1;
+    x1 = x2;
+    y1 = y2;
+    x2 = t1;
+    y2 = t2;
+  }
+  std::cout << std::fixed << std::showpoint;
+  std::cout << std::setprecision(size);
+  double answer1;
+  double answer2;
+  double d = sqrt( ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) ) * ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) ) 
+  - 4 * (y2 * y2 - y1 * y1) * (x1 * x1 * y2 * y2 - x2 * x2 * y1 * y1) );
+  answer1 = ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) + d) / ( 2 * ( (y2 * y2 - y1 * y1) ) );
+  answer2 = ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) - d) / ( 2 * ( (y2 * y2 - y1 * y1) ) );
+  if ( answer2 > x1 && answer2 < x2 )
+  {
+    cout << answer2 << '\n';
+  }
+  else
+  {
+    cout << answer2 << '\n';
+  }
 }
