4b9133f77277f48c56134f4c9536540089bbb9d5
V-Martyanov <martyanov@phystech.edu>
2021-09-07 23:17:52 +0300

    добавил пробелы

--- 1A/main.cpp
+++ 1A/main.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-#include <math.h>
-#include <iomanip>
 #include <algorithm>
-using namespace std;
-const int size = 10;
-int main()
-{ 
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using std::cin;
+using std::cout;
+const int s = 10;
+int main() {
   double x1;
   double x2;
   double y1;
   double y2;
   cin >> x1 >> y1 >> x2 >> y2;
-  if (x1 > x2)
-  {
+  if (x1 > x2) {
     double t1 = x1;
     double t2 = y1;
     x1 = x2;
@@ -21,15 +20,15 @@ int main()
     y2 = t2;
   }
   std::cout << std::fixed << std::showpoint;
-  std::cout << std::setprecision(size);
+  std::cout << std::setprecision(s);
   double answer1;
   double answer2;
   double d = sqrt( ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) ) * ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) ) 
-  - 4 * (y2 * y2 - y1 * y1) * (x1 * x1 * y2 * y2 - x2 * x2 * y1 * y1) );
+  - 2 * 2 * (y2 * y2 - y1 * y1) * (x1 * x1 * y2 * y2 - x2 * x2 * y1 * y1) );
   answer1 = ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) + d) / ( 2 * ( (y2 * y2 - y1 * y1) ) );
   answer2 = ( (2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) - d) / ( 2 * ( (y2 * y2 - y1 * y1) ) );
-  if ( answer2 > x1 && answer2 < x2 )
-  {
+  //Сравнивание минимума на возможном отрезке
+  if ( answer2 > x1 && answer2 < x2 ) {
     cout << answer2 << '\n';
   }
   else
