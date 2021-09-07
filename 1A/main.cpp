92d25b99a49fba8267655bb4f6237a10c19d1611
V-Martyanov <martyanov@phystech.edu>
2021-09-08 00:48:00 +0300

    формат 2

--- 1A/main.cpp
+++ 1A/main.cpp
@@ -1,10 +1,15 @@
-#include <math.h>
-#include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
 using std::cin;
 using std::cout;
 double Kvadard(double x1, double y1, double x2, double y2) {
+  if (x1 == x2 && y1 == y2) {
+      return x1;
+  }
+  if (y2 == y1) {
+    return (x2 * x2 * y1 * y1 - x1 * x1 * y2 * y2) / (2 * x2 * y1 * y1 - 2 * x1 * y2 * y2);
+  }
   double answer1;
   double answer2;
   double d = sqrt(((2 * x1 * y2 * y2 - 2 * x2 * y1 * y1)) *
@@ -12,14 +17,15 @@ double Kvadard(double x1, double y1, double x2, double y2) {
                   2 * 2 * (y2 * y2 - y1 * y1) *
                       (x1 * x1 * y2 * y2 - x2 * x2 * y1 * y1));
   //Квадратное уравнение
-  answer1 =
-      ((2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) + d) / (2 * ((y2 * y2 - y1 * y1)));
-  answer2 =
-      ((2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) - d) / (2 * ((y2 * y2 - y1 * y1)));
+  answer1 = ((2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) + d) /
+      (2 * ((y2 * y2 - y1 * y1)));
+  answer2 = ((2 * x1 * y2 * y2 - 2 * x2 * y1 * y1) - d) /
+      (2 * ((y2 * y2 - y1 * y1)));
   double ans;
   if (answer2 > x1 && answer2 < x2) {
     ans = answer2;
-  } else {
+  }
+  else {
     ans = answer1;
   }
   return ans;
@@ -41,6 +47,6 @@ int main() {
   }
   std::cout << std::fixed << std::showpoint;
   std::cout << std::setprecision(s);
-  cout << Kvadard(x1, y1, x2, y2);
+  cout << Kvadard(x1, y1, x2, y2) << '\n';
   return 0;
 }
