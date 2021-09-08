06524f3648531fe85af88992d7a7f2922fe643cd
V-Martyanov <vladimirmartalex@gmail.com>
2021-09-08 14:27:28 +0000

    В терминале нет папки 1B, проблема только в это задаче, так что через редактор в gitlab

--- 1B/main.cpp
+++ 1B/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
-
+using std::cin;
+using std::cout;
+long long NOD(long long n1, long long n2) {
+  long long diva;
+  long long d = n1 - n2;
+  if (n1 == n2) {
+    return n1;
+  }
+  if (d < 0) {
+    d = - d;
+    diva = NOD(n1, d);
+  } else {
+    diva = NOD(n2, d);
+  }
+  return diva;
+}
+long long NOK(long long  n1, long long n2) {
+  return ((n1 * n2) / NOD(n1, n2));
+}
 int main() {
-  std::cout << "Hello, testing system!\n";
+  long long n1, n2;
+  cin >> n1 >> n2;
+  cout << NOK(n1, n2);
+  return 0;
 }
