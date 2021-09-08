aa32a24b916ae3079ae6681e92d281f120e41512
V-Martyanov <martyanov@phystech.edu>
2021-09-08 18:52:43 +0300

    Lets go

--- 1C/main.cpp
+++ 1C/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-
+#include <math.h>
+using std::cin;
+using std::cout;
 int main() {
-  std::cout << "Hello, testing system!\n";
+    int iterator;
+    cin >> iterator;
+    while(iterator != 0) {
+        int N;
+        cin >> N;
+        for(int divider = 2; divider <= sqrt(N); divider++) {
+            while(N % divider == 0) {
+                N /= divider;
+                cout << divider << " ";
+            }
+        }
+        if(N != 1) {
+            cout << N <<" ";
+        }
+        cout << '\n';
+        iterator--;
+    }
+    return 0;
 }
