0dafa1be167506a0d8c435ff832e531470d62a1e
V-Martyanov <martyanov@phystech.edu>
2021-09-09 22:36:15 +0300

    после коментов

--- 1C/main.cpp
+++ 1C/main.cpp
@@ -1,23 +1,29 @@
 #include <cmath>
 #include <iostream>
+
 using std::cin;
 using std::cout;
+
+int checking_divisors(int n) {
+  for (int divider = 2; divider <= sqrt(n); divider++) {
+    while (n % divider == 0) {
+      n /= divider;
+      cout << divider << " ";
+    }
+  }
+  return n;
+}
+
 int main() {
-  int iterator;
-  cin >> iterator;
-  while (iterator != 0) {
+  int number_of_requests;
+  cin >> number_of_requests;
+  while (number_of_requests != 0) {
     int n;
     cin >> n;
-    for (int divider = 2; divider <= sqrt(n); divider++) {
-      while (n % divider == 0) {
-        n /= divider;
-        cout << divider << " ";
-      }
-    }
+    n = checking_divisors(n);
     if (n != 1) {
       cout << n << " ";
     }
-    iterator--;
+    number_of_requests--;
   }
-  return 0;
 }
