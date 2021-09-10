95ad4c4fd197379df475f9b1ff87bfaa89f8eda9
V-Martyanov <martyanov@phystech.edu>
2021-09-11 00:50:15 +0300

    spaces, now in English

--- 1D/main.cpp
+++ 1D/main.cpp
@@ -21,7 +21,7 @@ bool BinarySearch(int* begin, int* end, int target) {
       if (*(begin) == target) {
         yes_or_no = true;
       }
-      // в данном случае position играет роль, был ли сдвиг, или нет   
+      // в данном случае position играет роль, был ли сдвиг, или нет
       if (*(end) == target && position == 1) {
         yes_or_no = true;
       }
