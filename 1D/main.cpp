9952c948e43300251522c98b1040090ab1e749a1
V-Martyanov <martyanov@phystech.edu>
2021-09-10 23:54:34 +0300

    формат, родной

--- 1D/main.cpp
+++ 1D/main.cpp
@@ -3,26 +3,26 @@
 
 using namespace std;
 
-// position служит для понимание что сдвинуто левая сторона(она же не включительно)
-
 bool BinarySearch(int* begin, int* end, int target) {
+  // position служит для понимание что сдвинуто левая сторона(она же не 
+  // включительно)
   int position = 0;
   int distance;
   bool yes_or_no = false;
-  while(1 == 1) {
+  while (1 == 1) {
     distance = abs(begin - end) / 2;
-    if( * (begin + distance) < target) {
+    if (*(begin + distance) < target) {
       begin += distance;
     } else {
       end -= distance;
       position = 1;
     }
     if(abs(begin - end) <= 1) {
-      if( * (begin) == target) {
+      if(*(begin) == target) {
         yes_or_no = true;
       }
       // в данном случае position играет роль, был ли сдвиг, или нет
-      if( * (end) == target && position == 1) {
+      if(*(end) == target && position == 1) {
         yes_or_no = true;
       }
       return yes_or_no;
@@ -32,21 +32,19 @@ bool BinarySearch(int* begin, int* end, int target) {
 
 int main() {
   int array_length;
-  bool yes_or_no;
   cin >> array_length;
-  int *arr = new int[array_length];
-  for(int i = 0; i < array_length; i++) {
+  int* arr = new int[array_length];
+  for (int i = 0; i < array_length; i++) {
     cin >> arr[i];
   }
   int q;
   cin >> q;
-  while(q != 0) {
+  while (q != 0) {
     int arr_index_l;
     int arr_index_r;
     int targ;
     cin >> arr_index_l >> arr_index_r >> targ;
-    yes_or_no = BinarySearch((arr + arr_index_l), (arr + arr_index_r), targ);
-    if(yes_or_no) {
+    if(BinarySearch((arr + arr_index_l), (arr + arr_index_r), targ)) {
       cout << "YES";
     } else {
       cout << "NO";
@@ -54,5 +52,5 @@ int main() {
     cout << '\n';
     q--;
   }
-  delete [] arr;
+  delete[] arr;
 }
