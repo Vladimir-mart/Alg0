af84c9b587482fc77ed19460bd5e6b4439b0b798
V-Martyanov <martyanov@phystech.edu>
2021-09-11 01:55:58 +0300

    lets go

--- 1E/main.cpp
+++ 1E/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
-
+#include <math.h>
+#include <iomanip>
+using namespace std;
 int main() {
-  std::cout << "Hello, testing system!\n";
+  long long arr_length;
+  cin >> arr_length;
+  long double* arr = new long double[arr_length];
+  long double* special_arr = new long double[arr_length];
+  for (long long i = 0; i < arr_length; i++) {
+    cin >> arr[i];
+    if (i == 0) {
+      special_arr[0] = log(arr[0]);
+    } else {
+      special_arr[i] = special_arr[i-1]+log(arr[i]);
+    }
+  }
+  long long q;
+  cin >> q;
+  long double e = exp(1.0);
+  long long arr_index_l;
+  long long arr_index_r;
+  cout << fixed << showpoint;
+  cout << setprecision(7);
+  for(long long i = 0; i < q; i++) {
+    cin >> arr_index_l >> arr_index_r;
+    if(arr_index_l == 0) {
+      cout << pow(exp(1.0), special_arr[arr_index_r] / (arr_index_r-arr_index_l + 1) ) <<'\n';
+    } else {
+      cout << pow(exp(1.0), (special_arr[arr_index_r] - special_arr[arr_index_l - 1]) / (arr_index_r - arr_index_l + 1)) << '\n';
+    }
+  }
+  delete[] arr;
+  delete[] special_arr;
 }
