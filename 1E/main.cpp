3d88920aa804f020a457718095085c69d1ab33c2
V-Martyanov <martyanov@phystech.edu>
2021-09-13 01:59:21 +0300

    after commit

--- 1E/main.cpp
+++ 1E/main.cpp
@@ -1,24 +1,34 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
-using namespace std;
-void Exponentiation(const long double* arr) {
+
+using std::cout;
+using std::cin;
+using std::log;
+using std::pow;
+
+void Exponentiation(const long double* arr, const long long arr_index_l,
+                    const long long arr_index_r) {
+  if (arr_index_l == 0) {
+    cout << pow(exp(1.0), arr[arr_index_r] / (arr_index_r - arr_index_l + 1))
+          << '\n';
+  } else {
+    cout << pow(exp(1.0), (arr[arr_index_r] - arr[arr_index_l - 1]) /
+                              (arr_index_r - arr_index_l + 1))
+          << '\n';
+  }
+}
+
+void QueriesForTheGeometricMean(const long double* arr) {
   long long q;
   cin >> q;
   long long arr_index_l;
   long long arr_index_r;
-  cout << fixed << showpoint;
-  cout << setprecision(2 + 2 + 2 + 1);
+  cout << std::fixed << std::showpoint;
+  cout << std::setprecision(2 + 2 + 2 + 1);
   for (long long i = 0; i < q; i++) {
     cin >> arr_index_l >> arr_index_r;
-    if (arr_index_l == 0) {
-      cout << pow(exp(1.0), arr[arr_index_r] / (arr_index_r - arr_index_l + 1))
-           << '\n';
-    } else {
-      cout << pow(exp(1.0), (arr[arr_index_r] - arr[arr_index_l - 1]) /
-                                (arr_index_r - arr_index_l + 1))
-           << '\n';
-    }
+    Exponentiation(arr, arr_index_l, arr_index_r);
   }
 }
 int main() {
@@ -34,7 +44,7 @@ int main() {
       special_arr[i] = special_arr[i - 1] + log(arr[i]);
     }
   }
-  Exponentiation(special_arr);
+  QueriesForTheGeometricMean(special_arr);
   delete[] arr;
   delete[] special_arr;
 }
