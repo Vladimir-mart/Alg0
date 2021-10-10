d7dd829f0e24cf6f18a8d227a19f78912e5c7f38
V-Martyanov <martyanov@phystech.edu>
2021-10-10 17:28:53 +0300

    free it a pain

--- 2C/main.cpp
+++ 2C/main.cpp
@@ -9,49 +9,57 @@ using std::cout;
 using std::stack;
 using std::swap;
 using std::vector;
-void Split(vector<int>& arr1, vector<int>& arr2, int nres,
-           vector<int>& arr_res) {
-  unsigned int iter = 0;
-  unsigned int iter2 = 0;
-  for (int i = 0; i < nres; i++) {
-    if ((arr2[iter2] < arr1[iter]) && (iter2 < arr2.size())) {
-      arr_res.push_back(arr2[iter2]);
-      cout << arr2[iter2] << " ";
-      iter2++;
+void Split(vector<int>& arr1, vector<int>& arr2, vector<int>& arr_res) {
+  int n1 = arr1.size();
+  int n2 = arr2.size();
+  for (int i = 0, iter = 0, iter2 = 0; i < (n1 + n2); i++) {
+    if ((iter2 < n2)) {
+      if ((arr2[iter2] < arr1[iter])) {
+        arr_res.push_back(arr2[iter2]);
+        iter2++;
+      } else {
+        if (iter < n1) {
+          arr_res.push_back(arr1[iter]);
+          iter++;
+        } else {
+          arr_res.push_back(arr2[iter2]);
+          iter2++;
+        }
+      }
     } else {
-      if (iter < arr1.size()) {
+      if (iter < n1) {
         arr_res.push_back(arr1[iter]);
-        cout << arr1[iter] << " ";
+        ;
         iter++;
       } else {
         arr_res.push_back(arr2[iter2]);
-        cout << arr2[iter2] << " ";
         iter2++;
       }
     }
   }
 }
 int main() {
-  int n1;
+  int n1 = 0;
   vector<int> arr1;
   cin >> n1;
-  int temp_arr;
+  int temp_arr = 0;
   for (int i = 0; i < n1; i++) {
     cin >> temp_arr;
     arr1.push_back(temp_arr);
   }
-  int n2;
+  int n2 = 0;
   cin >> n2;
-  int nres = n1 + n2;
-  vector<int> arr_res;
   vector<int> arr2;
+  vector<int> arr_res;
   for (int i = 0; i < n2; i++) {
     cin >> temp_arr;
     arr2.push_back(temp_arr);
   }
-  Split(arr1, arr2, nres, arr_res);
+  Split(arr1, arr2, arr_res);
+  for (unsigned int i = 0; i < arr_res.size(); ++i) {
+    cout << arr_res[i] << " ";
+  }
   arr1.clear();
   arr2.clear();
-  arr_res.clear();
   cout << '\n';
 }
