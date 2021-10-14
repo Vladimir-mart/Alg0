04be8cee8c162e8b613424a5d59b99837ff2b61c
V-Martyanov <vladimirmartalex@gmail.com>
2021-10-14 18:39:19 +0000

    space

--- 2A/main.cpp
+++ 2A/main.cpp
@@ -32,10 +32,10 @@ void QuickSort(vector<T>& mas, int left, int right) {
 
   if (left < j) {
     QuickSort(mas, left, j);
-  } 
+  }
   if (i < right) {
     QuickSort(mas, i, right);
-  } 
+  }
 }
 
 int main() {
