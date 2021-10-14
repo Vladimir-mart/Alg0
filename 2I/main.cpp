0638b210a6c0a7ce77d94555fd53658366ddf067
V-Martyanov <martyanov@phystech.edu>
2021-10-14 22:58:14 +0300

    (

--- 2I/main.cpp
+++ 2I/main.cpp
@@ -1,5 +1,167 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::max;
+using std::swap;
+using std::vector;
+
+template <typename T>
+void QuickSort(vector<T>& mas, unsigned int left, unsigned int right) {
+  unsigned int i = left;
+  unsigned int j = right;
+  unsigned int mid = mas[(i + j) / 2].y;
+  do {
+    while (mas[i].y < mid) {
+      i++;
+    }
+    while (mas[j].y > mid) {
+      j--;
+    }
+    if (i <= j) {
+      swap(mas[i], mas[j]);
+      i++;
+      if (j > 0) {
+        j--;
+      }
+    }
+  } while (i <= j);
+  if (left < j) {
+    QuickSort(mas, left, j);
+  }
+  if (i < right) {
+    QuickSort(mas, i, right);
+  }
+}
+
+class BinaryHeap {
+ public:
+  BinaryHeap(/* args */);
+  void Extractmin();       //+
+  int GetMin();            //+
+  void Insert(int value);  //+
+  bool IsEmpty() const;    //+
+  size_t Size() const;     //+
+  void Clear();            //+
+  void Show();
+  ~BinaryHeap();
+  vector<int> a;
+
+ private:
+  void SiftDown(int index);             //+
+  void SiftUp(int index);               //+
+  static int GetLeftChild(int index);   //+
+  static int GetRightChild(int index);  //+
+  static int GetPerent(int index);      //+
+};
+
+void BinaryHeap::Show() {
+  cout << " Show ";
+  for (unsigned int i = 0; i < a.size(); i++) {
+    cout << a[i] << " ";
+  }
+  cout << '\n';
+}
+
+void BinaryHeap::Clear() { a.clear(); }
+
+int BinaryHeap::GetMin() { return a[0]; }
+
+size_t BinaryHeap::Size() const { return a.size(); }
+
+bool BinaryHeap::IsEmpty() const { return !a.empty(); }
+
+void BinaryHeap::Insert(int value) {
+  if (a.size() == 1) {
+    if (a[0] == 0) {
+      a[0] = value;
+      return;
+    }
+  }
+  a.push_back(value);
+  SiftUp(a.size() - 1);
+}
+
+int BinaryHeap::GetPerent(int index) { return (index - 1) / 2; }
+
+int BinaryHeap::GetLeftChild(int index) { return 2 * index + 1; }
+
+int BinaryHeap::GetRightChild(int index) { return 2 * index + 2; }
+
+void BinaryHeap::Extractmin() {
+  swap(a[0], a[a.size() - 1]);
+  a.pop_back();
+
+  SiftDown(0);
+}
+
+void BinaryHeap::SiftUp(int index) {
+  int perent = GetPerent(index);
+  while (perent >= 0 && a[perent] > a[index]) {
+    swap(a[perent], a[index]);
+    index = perent;
+    perent = GetPerent(index);
+  }
+}
+void BinaryHeap::SiftDown(int i) {
+  while ((2 * i + 1) <
+         (int)a.size()) {  // heapSize — количество элементов в куче
+    int left = GetLeftChild(i);    // left — левый сын
+    int right = GetRightChild(i);  // right — правый сын
+    int j = left;
+    if (right < (int)a.size() && a[right] < a[left]) {
+      j = right;
+    }
+    if (a[i] <= a[j]) {
+      break;
+    }
+    swap(a[i], a[j]);
+    i = j;
+  }
+}
+
+BinaryHeap::BinaryHeap(/* args */) {}
+
+BinaryHeap::~BinaryHeap() {}
+
+struct Point {
+  unsigned int x;
+  unsigned int y;
+};
+
+template <typename T>
+long long Maximum(vector<T>& coord, unsigned int size, unsigned int k) {
+  BinaryHeap heap;
+  long long ans = 0;
+  QuickSort(coord, 0, size - 1);
+  while (size != 0) {
+    heap.Insert(coord[size - 1].x);
+    if (heap.Size() > k) {
+      heap.Extractmin();
+    }
+    if (heap.Size() == k) {
+      ans = max(ans, (long long)(coord[size - 1].y * heap.GetMin()));
+    }
+    size--;
+  }
+  return ans;
+}
 
 int main() {
-  std::cout << "Hello, testing system!\n";
+  unsigned int n;
+  unsigned int k;
+  cin >> n >> k;
+  vector<Point> coord;
+  Point temp;
+  int x;
+  int y;
+  for (unsigned int i = 0; i < n; ++i) {
+    cin >> x >> y;
+    temp.x = x;
+    temp.y = y;
+    coord.push_back(temp);
+  }
+  cout << Maximum(coord, n, k) << '\n';
 }
