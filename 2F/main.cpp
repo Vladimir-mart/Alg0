037462d886e65c3f6f5642dd9d17f6191664a7f2
V-Martyanov <martyanov@phystech.edu>
2021-10-16 02:17:43 +0300

    update

--- 2F/main.cpp
+++ 2F/main.cpp
@@ -1,23 +1,19 @@
-#include <math.h>
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-#include <math.h>
-
 #include <cmath>
 #include <iostream>
 #include <stack>
+#include <vector>
+
 using std::cin;
 using std::cout;
 using std::stack;
 using std::swap;
+using std::vector;
 
-void QuickSort(int mas[], int size) {
-  int i = 0;
-  int j = size - 1;
-  int mid = mas[size / 2];
+template <typename T>
+void QuickSort(vector<T>& mas, int left, int right) {
+  int i = left;
+  int j = right;
+  int mid = mas[(i + j) / 2];
   do {
     while (mas[i] < mid) {
       i++;
@@ -33,23 +29,25 @@ void QuickSort(int mas[], int size) {
       j--;
     }
   } while (i <= j);
-  if (j > 0) {
-    QuickSort(mas, j + 1);
+
+  if (left < j) {
+    QuickSort(mas, left, j);
   }
-  if (i < size) {
-    QuickSort(&mas[i], size - i);
+  if (i < right) {
+    QuickSort(mas, i, right);
   }
 }
 
-int Counting(const int time[], const int a[], int n) {
+template <typename T>
+int Counting(vector<T>& time_end, const vector<T>& time_begin, int n) {
   int step = 0;
   int res = 0;
   int res_m = 0;
   for (int i = 0; i < n; i++) {
     res++;
-    if (time[step] <= a[i]) {
+    if (time_end[step] <= time_begin[i]) {
       if (step < n) {
-        while (time[step] <= a[i]) {
+        while (time_end[step] <= time_begin[i]) {
           res--;
           step++;
         }
@@ -65,17 +63,17 @@ int Counting(const int time[], const int a[], int n) {
 int main() {
   int n;
   cin >> n;
-  int* a = new int[n];
-  int* time = new int[n];
+  vector<int> time_begin;
+  vector<int> time_end;
+  int temp_b;
+  int temp_e;
   for (int i = 0, j = 0; i < n; i++, j++) {
-    cin >> a[i];
-    cin >> time[j];
-    time[j] += a[i];
+    cin >> temp_b >> temp_e;
+    time_begin.push_back(temp_b);
+    time_end.push_back(time_end[i] + temp_e);
   }
-  QuickSort(a, n);
-  QuickSort(time, n);
-  cout << Counting(time, a, n) << '\n';
-  delete[] a;
-  delete[] time;
+  QuickSort(time_begin, 0, n - 1);
+  QuickSort(time_end, 0, n - 1);
+  cout << Counting(time_end, time_begin, n) << '\n';
   return 0;
 }
