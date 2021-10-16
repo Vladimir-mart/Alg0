8bbc2f19487f57d04425567b197337627e7c6cb8
V-Martyanov <martyanov@phystech.edu>
2021-10-16 12:49:49 +0300

    template

--- 2I/main.cpp
+++ 2I/main.cpp
@@ -36,6 +36,7 @@ void QuickSort(vector<T>& mas, unsigned int left, unsigned int right) {
   }
 }
 
+template <typename T>
 class BinaryHeap {
  public:
   BinaryHeap(/* args */);
@@ -47,17 +48,18 @@ class BinaryHeap {
   void Clear();            //+
   void Show();
   ~BinaryHeap();
-  vector<int> a;
+  vector<T> a;
 
  private:
-  void SiftDown(int index);             //+
-  void SiftUp(int index);               //+
+  void SiftDown(unsigned int index);    //+
+  void SiftUp(unsigned int index);      //+
   static int GetLeftChild(int index);   //+
   static int GetRightChild(int index);  //+
   static int GetPerent(int index);      //+
 };
 
-void BinaryHeap::Show() {
+template <typename T>
+void BinaryHeap<T>::Show() {
   cout << " Show ";
   for (unsigned int i = 0; i < a.size(); i++) {
     cout << a[i] << " ";
@@ -65,15 +67,28 @@ void BinaryHeap::Show() {
   cout << '\n';
 }
 
-void BinaryHeap::Clear() { a.clear(); }
+template <typename T>
+void BinaryHeap<T>::Clear() {
+  a.clear();
+}
 
-int BinaryHeap::GetMin() { return a[0]; }
+template <typename T>
+int BinaryHeap<T>::GetMin() {
+  return a[0];
+}
 
-size_t BinaryHeap::Size() const { return a.size(); }
+template <typename T>
+size_t BinaryHeap<T>::Size() const {
+  return a.size();
+}
 
-bool BinaryHeap::IsEmpty() const { return !a.empty(); }
+template <typename T>
+bool BinaryHeap<T>::IsEmpty() const {
+  return !a.empty();
+}
 
-void BinaryHeap::Insert(int value) {
+template <typename T>
+void BinaryHeap<T>::Insert(int value) {
   if (a.size() == 1) {
     if (a[0] == 0) {
       a[0] = value;
@@ -84,20 +99,31 @@ void BinaryHeap::Insert(int value) {
   SiftUp(a.size() - 1);
 }
 
-int BinaryHeap::GetPerent(int index) { return (index - 1) / 2; }
+template <typename T>
+int BinaryHeap<T>::GetPerent(int index) {
+  return (index - 1) / 2;
+}
 
-int BinaryHeap::GetLeftChild(int index) { return 2 * index + 1; }
+template <typename T>
+int BinaryHeap<T>::GetLeftChild(int index) {
+  return 2 * index + 1;
+}
 
-int BinaryHeap::GetRightChild(int index) { return 2 * index + 2; }
+template <typename T>
+int BinaryHeap<T>::GetRightChild(int index) {
+  return 2 * index + 2;
+}
 
-void BinaryHeap::Extractmin() {
+template <typename T>
+void BinaryHeap<T>::Extractmin() {
   swap(a[0], a[a.size() - 1]);
   a.pop_back();
 
   SiftDown(0);
 }
 
-void BinaryHeap::SiftUp(int index) {
+template <typename T>
+void BinaryHeap<T>::SiftUp(unsigned int index) {
   int perent = GetPerent(index);
   while (perent >= 0 && a[perent] > a[index]) {
     swap(a[perent], a[index]);
@@ -105,9 +131,10 @@ void BinaryHeap::SiftUp(int index) {
     perent = GetPerent(index);
   }
 }
-void BinaryHeap::SiftDown(int i) {
-  while ((2 * i + 1) <
-         (int)a.size()) {  // heapSize — количество элементов в куче
+
+template <typename T>
+void BinaryHeap<T>::SiftDown(unsigned int i) {
+  while ((2 * i + 1) < a.size()) {  // heapSize — количество элементов в куче
     int left = GetLeftChild(i);    // left — левый сын
     int right = GetRightChild(i);  // right — правый сын
     int j = left;
@@ -122,9 +149,11 @@ void BinaryHeap::SiftDown(int i) {
   }
 }
 
-BinaryHeap::BinaryHeap(/* args */) {}
+template <typename T>
+BinaryHeap<T>::BinaryHeap(/* args */) {}
 
-BinaryHeap::~BinaryHeap() {}
+template <typename T>
+BinaryHeap<T>::~BinaryHeap() {}
 
 struct Point {
   unsigned int x;
@@ -133,7 +162,7 @@ struct Point {
 
 template <typename T>
 long long Maximum(vector<T>& coord, unsigned int size, unsigned int k) {
-  BinaryHeap heap;
+  BinaryHeap<int> heap;
   long long ans = 0;
   QuickSort(coord, 0, size - 1);
   while (size != 0) {
@@ -165,4 +194,3 @@ int main() {
   }
   cout << Maximum(coord, n, k) << '\n';
 }
-// deletions?
